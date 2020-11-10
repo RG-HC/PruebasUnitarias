18581976a76d3420b9749ddbe933b19c9f4e6c9b
RICARDO <RICARDO@RICARDOGHC>
2020-11-09 23:56:34 -0600

    Pruebas

---
+++ src/main/java/com/mycompany/pruebasunitarias/MetodosPrueba.java
@@ -0,0 +1,22 @@
+package com.mycompany.pruebasunitarias;
+
+public class MetodosPrueba {
+    
+    public boolean lessThanOrEqualToZero(int value){
+        return value <= 0;
+    }
+    
+    public String stutter(String word){
+        String repeat = "" + word.charAt(0) + word.charAt(1) + ".. ";
+        return repeat + repeat + word + "?";
+    }
+    
+    public int totalCups(int coups){
+        int freeCoups = coups / 6;
+        return coups + freeCoups;
+    }
+    
+    public int countWords(String word){
+        return word.split("\\s+|\n|,").length;
+    }
+}
